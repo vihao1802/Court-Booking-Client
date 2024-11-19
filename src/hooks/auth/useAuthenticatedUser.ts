dbd91a232a73b2ca48bb90dde83c9a9ec2d031e2
vihao <tranvihao40@gmail.com>
2024-11-19 22:07:51 +0700

    refactor: update module path

--- src/hooks/auth/useAuthenticatedUser.ts
+++ src/hooks/auth/useAuthenticatedUser.ts
@@ -28,7 +28,6 @@ export function useAuthenticatedUser(options?: Partial<SWRConfiguration>) {
     {
       // initialData: getAuthenticatedUser(),
       ...options,
-      // revalidateOnFocus: false,
     }
   );
 
