d84a6bd203dc6c20a9600df1c9ff00c133288f87
vihao <tranvihao40@gmail.com>
2024-11-19 22:07:00 +0700

    refactor(hook): reformat useGetCourtById

--- src/hooks/court/useGetCourtById.ts
+++ src/hooks/court/useGetCourtById.ts
@@ -1,17 +1,17 @@
-import { QueryKeys } from '@/constants/query-keys';
-import useSWR from 'swr';
-import { courtApi } from '@/api/court';
+import { QueryKeys } from "@/constants/query-keys";
+import useSWR from "swr";
+import { courtApi } from "@/api/court";
 
+export function useGetCourtById({ courtId }: { courtId: string }) {
+  const swrResponse = useSWR(
+    [QueryKeys.GET_COURT_LIST, courtId],
+    () => courtApi.getById(courtId),
+    {
+      dedupingInterval: 30 * 1000, // 30s
+      keepPreviousData: true,
+      fallbackData: undefined,
+    }
+  );
 
-export function useGetCourtById({ courtId } : { courtId: string }) {
-    const swrResponse = useSWR([QueryKeys.GET_COURT_LIST, courtId],
-        () => courtApi.getById(courtId),
-        {
-			dedupingInterval: 30 * 1000, // 30s
-			keepPreviousData: true,
-			fallbackData: null,
-		}
-    )
-
-    return swrResponse;
+  return swrResponse;
 }
