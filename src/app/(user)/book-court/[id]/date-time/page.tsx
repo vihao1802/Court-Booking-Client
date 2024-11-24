b09d0b9b3b5acb402566dec961a243ee52a82f72
vihao <tranvihao40@gmail.com>
2024-11-24 07:33:48 +0700

    refactor: remove unused modules

--- src/app/(user)/book-court/[id]/date-time/page.tsx
+++ src/app/(user)/book-court/[id]/date-time/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 import React from "react";
-import Stepper from "@mui/material/Stepper";
-import Step from "@mui/material/Step";
-import { Box, StepLabel } from "@mui/material";
+import { Box } from "@mui/material";
 import BookingDetails from "@/components/book-court/date-time/BookingDetails";
-import { ColorlibStepIcon } from "@/components/styles/StepperStyles";
 import StepperPayment from "@/components/book-court/payment/StepperPayment";
-
-const steps = ["Đặt lịch", "Thanh toán", "Hoàn tất"];
 
 const BookCourtDateTimePage = () => {
   return (
@@ -17,46 +12,6 @@ const BookCourtDateTimePage = () => {
       }}
     >
       <StepperPayment activeCurrStep={0} />
-      {/* <Stepper
-        activeStep={0}
-        sx={{
-          "& .Mui-active": {
-            "& .MuiStepIcon-root": {
-              color: "var(--buttonColor)",
-            },
-            "& .MuiStepConnector-line": {
-              color: "var(--buttonColor)",
-            },
-          },
-          "& .Mui-completed": {
-            "& .MuiStepIcon-root": {
-              color: "var(--buttonColor)",
-            },
-            "& .MuiStepConnector-line": {
-              borderColor: "var(--buttonColor)",
-            },
-          },
-          marginBottom: "20px",
-        }}
-      >
-        {steps.map((label, index) => {
-          const stepProps: { completed?: boolean } = {};
-          return (
-            <Step
-              key={label}
-              {...stepProps}
-              sx={{
-                padding: "5px",
-              }}
-            >
-              <StepLabel StepIconComponent={ColorlibStepIcon}>
-                {label}
-              </StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper> */}
-
       <BookingDetails />
     </Box>
   );
--- src/app/(user)/book-court/reservation/[id]/checkout/page.tsx
+++ src/app/(user)/book-court/reservation/[id]/checkout/page.tsx
@@ -1,57 +1,12 @@
 "use client";
 import React, { Fragment } from "react";
-import Stepper from "@mui/material/Stepper";
-import Step from "@mui/material/Step";
-import { StepLabel } from "@mui/material";
-import { ColorlibStepIcon } from "@/components/styles/StepperStyles";
 import PaymentDetail from "@/components/book-court/payment/PaymentDetail";
 import StepperPayment from "@/components/book-court/payment/StepperPayment";
-
-const steps = ["Đặt lịch", "Thanh toán", "Hoàn tất"];
 
 const BookCourtPaymentPage = () => {
   return (
     <Fragment>
       <StepperPayment activeCurrStep={2} />
-      {/* <Stepper
-        activeStep={2}
-        sx={{
-          "& .Mui-active": {
-            "& .MuiStepIcon-root": {
-              color: "var(--buttonColor)",
-            },
-            "& .MuiStepConnector-line": {
-              color: "var(--buttonColor)",
-            },
-          },
-          "& .Mui-completed": {
-            "& .MuiStepIcon-root": {
-              color: "var(--buttonColor)",
-            },
-            "& .MuiStepConnector-line": {
-              borderColor: "var(--buttonColor)",
-            },
-          },
-          marginBottom: "40px",
-        }}
-      >
-        {steps.map((label, index) => {
-          const stepProps: { completed?: boolean } = {};
-          return (
-            <Step
-              key={label}
-              {...stepProps}
-              sx={{
-                padding: "5px",
-              }}
-            >
-              <StepLabel StepIconComponent={ColorlibStepIcon}>
-                {label}
-              </StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper> */}
       <PaymentDetail />
     </Fragment>
   );
